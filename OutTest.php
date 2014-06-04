f4c8dc1bb014bd4eeb01a84b0c7a7c1fed55f812
Chris Johnson <cjohnson@tagged.com>
2014-06-04 10:59:50 -0700

    Simplifications and additions to unit tests

--- OutTest.php
+++ OutTest.php
@@ -5,73 +5,97 @@ require_once __DIR__.'/out.php';
 class OutTest extends PHPUnit_Framework_TestCase {
 
     public function outTestProvider() {
-        // replacement character
+        // unicode replacement character
         $textReplace = defined('ENT_SUBSTITUTE')  ? '�' : '';
         $htmlReplace = class_exists('UConverter') ? '�' : '';
 
         return array(
-            array("<>\"'&",    'text',   '&lt;&gt;&quot;&#039;&amp;'),
-            array("<>\"'&",    'html',    '<>"\'&'),
-            array("<>\"'&",    'binary', '<>"\'&'),
-            array("<>\"'&",    'script', '<>"\'&'),
-            array("<>\"'&",    'style',  '<>"\'&'),
-            array("<>\"'&",    'cdata',  '<>"\'&'),
-            array("foo bar 漢字 \xFF",   'text',   "foo bar 漢字 $textReplace"),
-            array("foo bar 漢字 \xFF",   'html',   "foo bar 漢字 $htmlReplace"),
-            array("foo bar 漢字 \xFF",   'binary', "foo bar 漢字 \xFF"),
-            array("foo bar 漢字 \xFF",   'script', "foo bar 漢字 $htmlReplace"),
-            array("foo bar 漢字 \xFF",   'style',  "foo bar 漢字 $htmlReplace"),
-            array("foo bar 漢字 \xFF",   'cdata',  "foo bar 漢字 $htmlReplace"),
+            array("<>\"'&", 'text',   '&lt;&gt;&quot;&#039;&amp;'),
+            array("<>\"'&", 'html',   '<>"\'&'),
+            array("<>\"'&", 'binary', '<>"\'&'),
+            array("<>\"'&", 'script', '<>"\'&'),
+            array("<>\"'&", 'style',  '<>"\'&'),
+            array("<>\"'&", 'cdata',  '<>"\'&'),
+            array("foo bar 漢字 \xFF", 'text',   "foo bar 漢字 $textReplace"),
+            array("foo bar 漢字 \xFF", 'html',   "foo bar 漢字 $htmlReplace"),
+            array("foo bar 漢字 \xFF", 'binary', "foo bar 漢字 \xFF"),
+            array("foo bar 漢字 \xFF", 'script', "foo bar 漢字 $htmlReplace"),
+            array("foo bar 漢字 \xFF", 'style',  "foo bar 漢字 $htmlReplace"),
+            array("foo bar 漢字 \xFF", 'cdata',  "foo bar 漢字 $htmlReplace"),
         );
     }
 
     /**
      * @dataProvider outTestProvider
      */
-    public function testSOut($input, $func, $expect) {
+    public function testSOutFunctions($input, $func, $expect) {
         $outfunc = "out\\s$func";
         $output = $outfunc($input);
-        $this->assertEquals($expect, $output, "input='$input' func='out\\$func' expect='$expect' output='$output'");
+        $this->assertEquals($expect, $output, "$outfunc($input) produced '$output', but expected '$expect'");
     }
 
     /**
      * @dataProvider outTestProvider
      */
-    public function testOut($input, $func, $expect) {
+    public function testOutFunctions($input, $func, $expect) {
         $outfunc = "out\\$func";
         ob_start();
         $outfunc($input);
         $output = ob_get_clean();
-        $this->assertEquals($expect, $output, "input='$input' func='out\\$func' expect='$expect' output='$output'");
+        $this->assertEquals($expect, $output, "$outfunc($input) produced '$output', but expected '$expect'");
     }
 
-    public function testOutScriptInvalidTerminator() {
-        try {
-            $input = 'foo = "bar";</script ><script>alert(666);</script >';
-            out\script($input);
-            $this->fail('Expected script terminator to throw');
-        } catch (out\OutException $e) {
-            $this->assertContains($input, $e->getMessage());
-        }
+    /**
+     * @expectedException        out\OutException
+     * @expectedExceptionMessage </script
+     */
+    public function testOutSScriptWithScriptTerminator() {
+        $input = '</script';
+        out\sscript($input);
     }
 
-    public function testOutStyleInvalidTerminator() {
-        try {
-            $input = 'background-color: red;</style ><script>alert(666);</script>';
-            out\style($input);
-            $this->fail('Expected style terminator to throw');
-        } catch (out\OutException $e) {
-            $this->assertContains($input, $e->getMessage());
-        }
+    /**
+     * @expectedException        out\OutException
+     * @expectedExceptionMessage </style
+     */
+    public function testOutSStyleWithStyleTerminator() {
+        $input = '</style';
+        out\sstyle($input);
     }
 
-    public function testOutCdataInvalidTerminator() {
-        try {
-            $input = 'background-color: red;]]><script>alert(666);</script>';
-            out\cdata($input);
-            $this->fail('Expected style terminator to throw');
-        } catch (out\OutException $e) {
-            $this->assertContains($input, $e->getMessage());
-        }
+    /**
+     * @expectedException        out\OutException
+     * @expectedExceptionMessage ]]>
+     */
+    public function testOutSCDataWithCDataTerminator() {
+        $input = ']]>';
+        out\scdata($input);
+    }
+
+    /**
+     * @expectedException        out\OutException
+     * @expectedExceptionMessage foo = "bar";</script ><script>alert(666);</script >
+     */
+    public function testOutScriptWithScriptTerminator() {
+        $input = 'foo = "bar";</script ><script>alert(666);</script >';
+        out\script($input);
+    }
+
+    /**
+     * @expectedException        out\OutException
+     * @expectedExceptionMessage color: red;</style ><script>alert(666);</script>
+     */
+    public function testOutStyleWithStyleTerminator() {
+        $input = 'color: red;</style ><script>alert(666);</script>';
+        out\style($input);
+    }
+
+    /**
+     * @expectedException        out\OutException
+     * @expectedExceptionMessage background-color: red;]]><script>alert(666);</script>
+     */
+    public function testOutCDataWithCDataTerminator() {
+        $input = 'background-color: red;]]><script>alert(666);</script>';
+        out\cdata($input);
     }
 }
